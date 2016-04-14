d4a0a0525633e84fcd65890d843d91318110e6f9
elelowwaydi <lloydaaron.barnacha@yahoo.com>
2016-04-14 14:44:24 +0800

    updated...

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
-  const $burgerOpen = $('.burger-open');
-  const $burgerClose = $('.burger-close');
-  $burgerOpen.click(function() {
-    $('html').addClass('toggle-nav overflow');
-    $(this).addClass('fade');
-    $burgerClose.removeClass('fade');
-  });
-  $burgerClose.click(function() {
-    $('html').removeClass('toggle-nav overflow');
-    $(this).addClass('fade');
-    $burgerOpen.removeClass('fade');
-  });
+    const $burgerOpen = $('.burger-open');
+    const $burgerClose = $('.burger-close');
+    const $html = $('html');
+    $burgerOpen.click(function() {
+        $html.addClass('toggle-nav overflow');
+        $(this).addClass('fade');
+        $burgerClose.removeClass('fade');
+    });
+    $burgerClose.click(function() {
+        $html.removeClass('toggle-nav');
+        $(this).addClass('fade');
+        $burgerOpen.removeClass('fade');
+        setTimeout(function() {
+            $html.removeClass("overflow");
+        }, 500);
+    });
 });
